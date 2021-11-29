06d58e411516a7104246cc2012fd0be487a46e15
Spencer Posner <spencer.posner11@outlook.com>
2021-11-28 19:50:57 -0500

    finished tutorial

---
+++ pet-shop-tutorial/contracts/Adoption.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.5.0;
+
+contract Adoption {
+    address[16] public adopters;
+
+    // request to adopt a pet (caller of smart contract)
+    function adopt(uint256 petId) public returns (uint256) {
+        require(petId >= 0 && petId <= 15);
+
+        adopters[petId] = msg.sender;
+
+        return petId;
+    }
+
+    function getAdopters() public view returns (address[16] memory) {
+        return adopters;
+    }
+}
