b6ac346c6c7742053fc8a786a8a91f3cc9897290
Riviere Fougy <rivierefougy@gmail.com>
2021-12-04 09:43:34 -0500

    installed ganache-cli

--- pet-shop-tutorial/contracts/HttToken.sol
+++ pet-shop-tutorial/contracts/HttToken.sol
@@ -4,8 +4,8 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract HttToken is ERC20 {
 
-  constructor(uint256 _supply) ERC20("HttToken", "HTT") {
-     _mint(msg.sender, _supply * (10 ** decimals()));
+  constructor(uint256 initialSupply) ERC20("HttToken", "HTT") {
+     _mint(msg.sender, initialSupply * (10 ** decimals()));
   }
 
 }
